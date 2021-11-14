251accfa54bd44fa8407499731f6493ad866a8f1
Alen Šimec <alen@tvz.hr>
2021-11-14 12:40:01 +0100

    Update racun.php

--- racun.php
+++ racun.php
@@ -13,17 +13,17 @@
             <meta name="viewport" content="width=device-width, initial-scale=1">
             <link rel="shortcut icon" type="image/x-icon" href="favicon.ico">
         </head>
-    <body>
+    <body>';
     <?php
 			$_a=$_POST['a'];
 			$_b=$_POST['b'];
 			$c=(3*$_a-$_b)/2;
 			print '
-			
-			<p a='.$_a .'</p>
+			<p>a='.$_a .'</p>
 			<p>b='.$_b .'</p>
-			<p>c=(3*'.$_a.'-'.$_b.')/2='.$c .'</p>
+			<p>c=(3*'.$_a.'-'.$_b.')/2='.$c .'</p>';
 			
 	?>
+print '
 </body>
 </html>';
